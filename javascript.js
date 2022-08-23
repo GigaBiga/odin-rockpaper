6645999d6f6563dfc413d7f80b1d97459687a194
Rui Reis <ruimartinseis@gmail.com>
2022-08-23 16:21:49 +0100

    Make PlayerGame function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,56 @@
-const moves = ["Rock","Paper","Scissors"];
+const moves = ["ROCK","PAPER","SCISSORS"];
+var scoreHuman = 0;
+var scoreMachine = 0;
 
 function ComputerChoice(choices){
     return choices[Math.floor(Math.random() * 3)];
 }
-console.log(ComputerChoice(moves));
+
+function PlayerGame(ComputerChoice){
+    var userinput = prompt("Rock, Paper, or Scisors").toUpperCase();
+    if (userinput == 'ROCK'){
+        if ('ROCK' == ComputerChoice){
+            alert('Computer chose Rock, It\'s a draw');
+        }
+        else if (ComputerChoice == 'SCISSORS'){
+            alert('Computer chose Scissors, You win');
+            return scoreHuman++;
+        }
+        else{
+            alert('Computer chose Paper, You lose');
+            return scoreMachine++;
+        }
+    }
+    else if (userinput == 'PAPER'){
+        if ('PAPER' == ComputerChoice){
+            alert('Computer chose Paper, It\'s a draw');
+        }
+        else if (ComputerChoice == 'ROCK'){
+            alert('Computer chose Rock, You win');
+            return scoreHuman++;
+        }
+        else{
+            alert('Computer chose Scissors, You lose');
+            return scoreMachine++;
+        }
+    }
+    else if (userinput == 'SCISSORS'){
+        if ('Scissors' == ComputerChoice){
+            alert('Computer chose Scissors, It\'s a draw');
+        }
+        else if (ComputerChoice == 'PAPER'){
+            alert('Computer chose Paper, You win');
+            return scoreHuman++;
+        }
+        else{
+            alert('Computer chose Rock, You lose');
+            return scoreMachine++;
+        }
+    }
+    else {
+        alert('Incorect input, Try again')
+    }
+}   
+
+PlayerGame()
+alert('Your score is ' + scoreHuman +' Computers score '+scoreMachine);
